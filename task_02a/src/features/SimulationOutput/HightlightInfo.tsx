185908aa69e4755269fbdc521c424815f375a6c8
Florian Kapaun <hello@florian-kapaun.de>
2025-11-08 22:24:09 +0100

    :art: Inline Card Component into HighlightInfo

--- task_02a/src/features/SimulationOutput/HightlightInfo.tsx
+++ task_02a/src/features/SimulationOutput/HightlightInfo.tsx
@@ -1,5 +1,3 @@
-import Card from "../../components/Card"
-
 type HighlightInfoProps = {
     className?: string
 }
@@ -7,10 +5,24 @@ type HighlightInfoProps = {
 const HighlightInfo = ({ className }: HighlightInfoProps) => {
     return (
         <section className={`grid grid-cols-3 gap-4 ${className}`}>
-            <Card title="1896 kWh" subtitle="total energy charged p.a." />
-            <Card title="1.5hrs" subtitle="average charging time p.a." />
-            <Card title="250" subtitle="charging events p.a." />
+            <Highlight title="1896 kWh" subtitle="total energy charged p.a." />
+            <Highlight title="1.5hrs" subtitle="average charging time p.a." />
+            <Highlight title="250" subtitle="charging events p.a." />
         </section>
+    )
+}
+
+type HighlightProps = {
+    title: string
+    subtitle: string
+}
+
+const Highlight = ({ title, subtitle }: HighlightProps) => {
+    return (
+        <div>
+            <p className="text-5xl font-semibold leading-none">{title}</p>
+            <p className="text-sm text-gray-600">{subtitle}</p>
+        </div>
     )
 }
 
