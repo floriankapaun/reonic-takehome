6d9792b16cf078387b6b363e5713968a69e0229a
Florian Kapaun <hello@florian-kapaun.de>
2025-11-08 15:31:03 +0100

    :tada: Initialize react + tailwindcss project for task_02

---
+++ task_02a/eslint.config.js
@@ -0,0 +1,26 @@
+import js from "@eslint/js"
+import prettierConfig from "eslint-config-prettier"
+import prettier from "eslint-plugin-prettier"
+import simpleImportSort from "eslint-plugin-simple-import-sort"
+import tseslint from "typescript-eslint"
+
+export default [
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    prettierConfig,
+    {
+        files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+        languageOptions: {
+            parser: tseslint.parser,
+        },
+        plugins: {
+            "simple-import-sort": simpleImportSort,
+            prettier,
+        },
+        rules: {
+            "simple-import-sort/imports": "error",
+            "simple-import-sort/exports": "error",
+            "prettier/prettier": "error",
+        },
+    },
+]
---
+++ task_02a/prettier.config.js
@@ -0,0 +1,7 @@
+const config = {
+    semi: false,
+    tabWidth: 4,
+    printWidth: 100,
+}
+
+export default config
