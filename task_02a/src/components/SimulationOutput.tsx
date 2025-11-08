89fcaaa20c61d4eae3a6c0e62eea2e99a08b97cb
Florian Kapaun <hello@florian-kapaun.de>
2025-11-08 23:44:40 +0100

    :twisted_rightwards_arrows: Merge components

--- task_02a/src/components/SimulationOutput.tsx
+++ task_02a/src/components/SimulationOutput.tsx
@@ -59,68 +59,19 @@ const useGetMonthlyChargingActivity = () => {
     })
 }
 
-type ChargingActivityChartProps = {
-    timeframe: Timeframe
-}
-
-const ChargingActivityChart = ({ timeframe }: ChargingActivityChartProps) => {
-    const { data, isLoading, isFetching } = useGetMonthlyChargingActivity()
-
-    useEffect(() => {
-        // Just to simulate refetching, I'm not even switching the data.
-        queryClient.invalidateQueries({ queryKey: chargingActivityKeys.monthly() })
-    }, [timeframe])
-
-    return (
-        <LineChart
-            className="w-full aspect-29/9"
-            responsive
-            data={data}
-            margin={{ top: 10, right: 0, left: 0, bottom: 0 }}
-        >
-            <XAxis dataKey="month" />
-            <YAxis yAxisId="left" orientation="left" tickLine={false} />
-            <YAxis yAxisId="right" orientation="right" tickLine={false} />
-
-            {(isLoading || isFetching) && <Label value="Loading data..." position="center" />}
-
-            {!isLoading && !data && <Label value="No data available" position="center" />}
-
-            <Tooltip
-                formatter={(value, name) => {
-                    if (name === "kWh") return [`${value} kWh`, "Energy charged"]
-                    return [value, "Charging events"]
-                }}
-            />
-
-            <Line
-                yAxisId="left"
-                dot={false}
-                dataKey="kWh"
-                type="monotone"
-                stroke="#8884d8"
-                strokeWidth={2}
-                fill="url(#colorKWH)"
-            />
-            <Line
-                strokeWidth={2}
-                type="monotone"
-                dot={false}
-                yAxisId="right"
-                dataKey="charges"
-                stroke="#82ca9d"
-                fill="url(#colorCharges)"
-            />
-        </LineChart>
-    )
-}
-
 type ChargingActivityProps = {
     className?: string
 }
 
 const ChargingActivity = ({ className = "" }: ChargingActivityProps) => {
     const [timeframe, setTimeframe] = useState<Timeframe>("Month")
+
+    const { data, isLoading, isFetching } = useGetMonthlyChargingActivity()
+
+    useEffect(() => {
+        // Just to simulate refetching, I'm not even switching the data.
+        queryClient.invalidateQueries({ queryKey: chargingActivityKeys.monthly() })
+    }, [timeframe])
 
     return (
         <div className={className}>
@@ -133,7 +84,46 @@ const ChargingActivity = ({ className = "" }: ChargingActivityProps) => {
                 />
             </div>
 
-            <ChargingActivityChart timeframe={timeframe} />
+            <LineChart
+                className="w-full aspect-29/9"
+                responsive
+                data={data}
+                margin={{ top: 10, right: 0, left: 0, bottom: 0 }}
+            >
+                <XAxis dataKey="month" />
+                <YAxis yAxisId="left" orientation="left" tickLine={false} />
+                <YAxis yAxisId="right" orientation="right" tickLine={false} />
+
+                {(isLoading || isFetching) && <Label value="Loading data..." position="center" />}
+
+                {!isLoading && !data && <Label value="No data available" position="center" />}
+
+                <Tooltip
+                    formatter={(value, name) => {
+                        if (name === "kWh") return [`${value} kWh`, "Energy charged"]
+                        return [value, "Charging events"]
+                    }}
+                />
+
+                <Line
+                    yAxisId="left"
+                    dot={false}
+                    dataKey="kWh"
+                    type="monotone"
+                    stroke="#8884d8"
+                    strokeWidth={2}
+                    fill="url(#colorKWH)"
+                />
+                <Line
+                    strokeWidth={2}
+                    type="monotone"
+                    dot={false}
+                    yAxisId="right"
+                    dataKey="charges"
+                    stroke="#82ca9d"
+                    fill="url(#colorCharges)"
+                />
+            </LineChart>
         </div>
     )
 }
