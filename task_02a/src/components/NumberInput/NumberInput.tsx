35343e486725f852b5ecc46331e33cee9682a4ff
Florian Kapaun <hello@florian-kapaun.de>
2025-11-08 20:51:21 +0100

    :lipstick: Improve NumberInput style

--- task_02a/src/components/NumberInput/NumberInput.tsx
+++ task_02a/src/components/NumberInput/NumberInput.tsx
@@ -11,7 +11,7 @@ type NumberInputProps = {
 
 const NumberInput = ({ value, label, suffix, onChange, ...rest }: NumberInputProps) => {
     return (
-        <label className="w-full inline-flex items-center gap-3 py-2 hover:bg-gray-100 transition-colors duration-100">
+        <label className="w-full inline-flex items-center gap-3 px-3 py-2 rounded-sm hover:bg-gray-100 focus-within:bg-gray-200 focus-within:hover:bg-gray-200 transition-colors duration-100">
             <span className="flex-none text-sm leading-tight whitespace-nowrap overflow-hidden text-ellipsis max-w-[75%]">
                 {label}
             </span>
