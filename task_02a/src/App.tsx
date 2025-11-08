51db50f9f4c52735e8abab35c37e407f35494b35
Florian Kapaun <hello@florian-kapaun.de>
2025-11-08 22:01:00 +0100

    :lipstick: Refactor page layout

--- task_02a/src/App.tsx
+++ task_02a/src/App.tsx
@@ -1,27 +1,27 @@
-import { APITester } from "./APITester"
 import HighlightInfo from "./components/HightlightInfo"
-import InputParameters from "./components/InputParameters"
-import NumberInput from "./components/NumberInput/NumberInput"
 import "./index.css"
+import ChargingActivityChart from "./features/SimulationOutput/ChargingActivity/Chart"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import SimulationInputParameters from "./features/SimulationInput/Parameters"
+
+export const queryClient = new QueryClient()
 
 export function App() {
     return (
-        <>
-            <header className="col-span-4 p-4">
-                <hgroup>
-                    <h1 className="text-5xl font-semibold">EV Chargepoint Simulator</h1>
-                    <h2 className="text-2xl">Subtitle</h2>
-                </hgroup>
-            </header>
-            <aside className="col-span-1 p-4">
-                <h3>Simulation Parameters</h3>
-                <p>Adjust and run simulation</p>
-                <InputParameters />
+        <QueryClientProvider client={queryClient}>
+            <aside className="col-span-1 p-4 border-r border-gray-200">
+                <h1 className="text-xl font-semibold mb-6">EV Chargepoint Simulator</h1>
+                <h3 className="font-semibold">Simulation Parameters</h3>
+                <p className="text-xs text-gray-600">Adjust and run simulation</p>
+                <SimulationInputParameters className="sticky top-0 pt-4" />
             </aside>
-            <main className="col-span-3 bg-gray-100 p-6 rounded-t-sm shadow-md">
-                <HighlightInfo />
+            <main className="col-span-3  p-6 rounded-t-sm ">
+                <HighlightInfo className="mt-4" />
+
+                <h2 className="text-xl font-semibold mb-4 mt-10">Charging Activity</h2>
+                <ChargingActivityChart />
             </main>
-        </>
+        </QueryClientProvider>
     )
 }
 
