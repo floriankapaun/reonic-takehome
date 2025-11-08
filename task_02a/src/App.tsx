010a07a2694f7f88f5b96dcfb17750a05afabac3
Florian Kapaun <hello@florian-kapaun.de>
2025-11-08 22:24:42 +0100

    :lipstick: Page Layout touch ups

--- task_02a/src/App.tsx
+++ task_02a/src/App.tsx
@@ -1,4 +1,4 @@
-import HighlightInfo from "./components/HightlightInfo"
+import HighlightInfo from "./features/SimulationOutput/HightlightInfo"
 import "./index.css"
 import ChargingActivityChart from "./features/SimulationOutput/ChargingActivity/Chart"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
@@ -9,14 +9,19 @@ export const queryClient = new QueryClient()
 export function App() {
     return (
         <QueryClientProvider client={queryClient}>
-            <aside className="col-span-1 p-4 border-r border-gray-200">
-                <h1 className="text-xl font-semibold mb-6">EV Chargepoint Simulator</h1>
-                <h3 className="font-semibold">Simulation Parameters</h3>
-                <p className="text-xs text-gray-600">Adjust and run simulation</p>
-                <SimulationInputParameters className="sticky top-0 pt-4" />
+            <aside className="col-span-1 border-r border-gray-200">
+                <header className="p-4 border-b border-gray-200">
+                    <h1 className="text-xl font-semibold">EV Chargepoint Simulator</h1>
+                </header>
+
+                <h2 className="font-semibold px-4 pt-4">Simulation Parameters</h2>
+                <p className="text-xs text-gray-600 px-4 mb-5">Adjust and run simulation</p>
+
+                <SimulationInputParameters className="sticky top-0 px-4 " />
             </aside>
-            <main className="col-span-3  p-6 rounded-t-sm ">
-                <HighlightInfo className="mt-4" />
+
+            <main className="col-span-3 p-6">
+                <HighlightInfo className="mt-2" />
 
                 <h2 className="text-xl font-semibold mb-4 mt-10">Charging Activity</h2>
                 <ChargingActivityChart />
