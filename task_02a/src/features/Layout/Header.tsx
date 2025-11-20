6f015086787f99890d1c6142df7b73eeffa2c413
Florian Kapaun <hello@florian-kapaun.de>
2025-11-20 21:16:51 +0100

    :children_crossing: Add link to header

--- task_02a/src/features/Layout/Header.tsx
+++ task_02a/src/features/Layout/Header.tsx
@@ -1,8 +1,12 @@
+import { Link } from "@tanstack/react-router"
+
 const Header = () => {
     return (
         <header className="border-b border-l border-gray-200">
             <div className="max-w-7xl mx-auto px-4 py-3">
-                <h1 className="text-xl font-semibold">EV Chargepoint Simulator</h1>
+                <Link to="/">
+                    <h1 className="text-xl font-semibold">EV Chargepoint Simulator</h1>
+                </Link>
             </div>
         </header>
     )
