6d301655ba3755d7e5fcb164c782d736a40f9b66
Florian Kapaun <hello@florian-kapaun.de>
2025-11-22 10:27:58 +0100

    :recycle: Use new Button and remove few things

--- task_02a/src/routes/configuration.lazy.tsx
+++ task_02a/src/routes/configuration.lazy.tsx
@@ -1,10 +1,11 @@
+import Button from "@/components/Button"
 import NumberInput from "@/components/NumberInput"
 import Select from "@/components/Select"
 import TextInput from "@/components/TextInput"
 import { useConfigurationContext } from "@/features/Configuration"
 import ChargepointSetup from "@/features/Configuration/ChargepointSetup/ChargepointSetup"
 import useLocalStorage from "@/hooks/useLocalStorage"
-import { IconBolt, IconChargingPile, IconPlug, IconPlus, IconTrash } from "@tabler/icons-react"
+import { IconArrowLeft, IconBolt, IconCheck, IconTrash } from "@tabler/icons-react"
 import { createLazyFileRoute, Link } from "@tanstack/react-router"
 import { useState, type Dispatch, type SetStateAction } from "react"
 
@@ -97,7 +98,6 @@ const Zone = ({
 function ConfigurationPage() {
     const { id } = Route.useSearch()
     const { configurations, activeConfiguration } = useConfigurationContext()
-    const [zones, setZones] = useLocalStorage<ChargingZone[]>("chargingZones", [])
 
     const editConfigurationId = id ?? activeConfiguration?.id
 
@@ -110,20 +110,25 @@ function ConfigurationPage() {
         return <div>Configuration not found</div>
     }
 
-    const addZone = () => {
-        const newZone: ChargingZone = {
-            id: zones.length > 0 ? (zones[zones.length - 1]?.id ?? 0) + 1 : 1,
-            chargers: 1,
-            powerPerChargerInKW: 11,
-        }
-        setZones([...zones, newZone])
-    }
+    const hasChanges = false
 
     return (
-        <main className="min-h-(--content-min-height) flex flex-col justify-between">
-            <div className="max-w-7xl mx-auto h-full">
-                <div className="p-4">
+        <main className="min-h-(--content-min-height) flex flex-col justify-between gap-6">
+            <div className="max-w-7xl mx-auto">
+                <div className="flex flex-row justify-start items-center gap-4 p-4">
                     <h2 className="text-xl font-medium">Configuration</h2>
+
+                    <Link to="/" tabIndex={-1}>
+                        <Button
+                            size="xs"
+                            leftSection={<IconArrowLeft />}
+                            onClick={() => {
+                                // TODO: Prompt to save changes if any
+                            }}
+                        >
+                            Back to Simulation
+                        </Button>
+                    </Link>
                 </div>
 
                 <div className="grid grid-cols-1 lg:grid-cols-3 gap-x-4 gap-y-8 mb-2 p-4">
@@ -154,23 +159,33 @@ function ConfigurationPage() {
                 </div>
             </div>
 
-            <div className="sticky bottom-0 bg-white p-4 mt-6 border-t flex justify-end gap-2">
-                <Link to="/" tabIndex={-1}>
-                    <button
-                        className="px-4 py-2 bg-gray-200 rounded"
-                        type="button"
-                        onClick={() => {
-                            // TODO: Reset Form
-                            // TODO: Ask for confirmation if changes were made
-                        }}
-                    >
-                        Cancel
-                    </button>
-                </Link>
+            <div className="sticky bottom-0  bg-white border-t border-gray-200">
+                <div className="max-w-7xl mx-auto flex gap-4 p-4">
+                    {hasChanges ? (
+                        <>
+                            <Button color="blue" leftSection={<IconCheck />}>
+                                Save Changes
+                            </Button>
 
-                <button className="px-4 py-2 bg-blue-600 text-white rounded" type="button">
-                    Save Configuration
-                </button>
+                            <Link to="/" tabIndex={-1}>
+                                <Button
+                                    onClick={() => {
+                                        // TODO: Reset Form
+                                        // TODO: Ask for confirmation if changes were made
+                                    }}
+                                >
+                                    Cancel
+                                </Button>
+                            </Link>
+                        </>
+                    ) : (
+                        <Link to="/" tabIndex={-1}>
+                            <Button color="blue" leftSection={<IconBolt />}>
+                                Run Simulation
+                            </Button>
+                        </Link>
+                    )}
+                </div>
             </div>
         </main>
     )
