245204cb91fcc268b72c7bac6d7adce0a131a016
Florian Kapaun <hello@florian-kapaun.de>
2025-11-22 10:17:37 +0100

    :sparkles: Add color and size variants to button

--- task_02a/src/components/Button.tsx
+++ task_02a/src/components/Button.tsx
@@ -1,19 +1,38 @@
-import type { ReactNode } from "react"
+import type { ButtonHTMLAttributes, ReactNode } from "react"
 
-type ButtonProps = {
+const sizeStyles = {
+    xs: "h-7 gap-1.5 px-2.5 [&_svg]:w-4 has-[[data-section]_svg]:px-2",
+    sm: "h-8 gap-1.5 px-3 [&_svg]:w-4 has-[[data-section]_svg]:px-2.5",
+    md: "h-9 gap-2 px-4 py-2 [&_svg]:w-4 has-[[data-section]_svg]:px-3",
+} as const
+
+const colorStyles = {
+    gray: "bg-white border-gray-300 hover:bg-gray-100 focus-visible:border-blue-500 focus-visible:ring-blue-500/40",
+    blue: "bg-blue-600 border-transparent text-white hover:bg-blue-700 focus-visible:ring-blue-500/40",
+} as const
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
     children: ReactNode
+    color?: keyof typeof colorStyles
     leftSection?: ReactNode
-    onClick?: () => void
+    size?: keyof typeof sizeStyles
 }
 
-const Button = ({ children, leftSection, onClick }: ButtonProps) => {
+const Button = ({
+    children,
+    className,
+    color = "gray",
+    leftSection,
+    size = "md",
+    ...buttonProps
+}: ButtonProps) => {
     return (
         <button
             type="button"
-            className="px-2 py-1 text-sm font-medium text-blue-600 rounded-sm  leading-none hover:bg-blue-50 inline-flex items-center gap-1 cursor-pointer focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-1"
-            onClick={onClick}
+            className={`inline-flex items-center justify-center shrink-0 text-sm font-medium whitespace-nowrap border rounded-md outline-none shadow-xs transition-[color,box-shadow,translate] cursor-pointer focus-visible:ring-3 active:translate-y-[1px] ${sizeStyles[size]} ${colorStyles[color]} ${className ?? ""}`}
+            {...buttonProps}
         >
-            <span>{leftSection}</span>
+            {leftSection && <span data-section="left">{leftSection}</span>}
             <span>{children}</span>
         </button>
     )
