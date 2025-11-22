301f4133e5d6ed4f98c221b3b1228581f5b9688d
Florian Kapaun <hello@florian-kapaun.de>
2025-11-22 17:46:16 +0100

    :sparkles: Add subtle-gray style to button

--- task_02a/src/components/Button.tsx
+++ task_02a/src/components/Button.tsx
@@ -7,8 +7,10 @@ const sizeStyles = {
 } as const
 
 const colorStyles = {
-    gray: "bg-white border-gray-300 hover:bg-gray-100 focus-visible:border-blue-500 focus-visible:ring-blue-500/40",
-    blue: "bg-blue-600 border-transparent text-white hover:bg-blue-700 focus-visible:ring-blue-500/40",
+    gray: "bg-white border-gray-300 shadow-xs hover:bg-gray-100 focus-visible:border-blue-500 focus-visible:ring-blue-500/40",
+    "subtle-gray":
+        "bg-transparent border-transparent hover:bg-gray-200/30 focus-visible:bg-transparent focus-visible:border-blue-500 focus-visible:ring-blue-500/40",
+    blue: "bg-blue-600 border-transparent text-white shadow-xs hover:bg-blue-700 focus-visible:ring-blue-500/40",
 } as const
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
@@ -29,7 +31,7 @@ const Button = ({
     return (
         <button
             type="button"
-            className={`inline-flex items-center justify-center shrink-0 align-middle text-sm font-medium whitespace-nowrap border rounded-md outline-none shadow-xs transition-[color,box-shadow,translate] cursor-pointer focus-visible:ring-3 active:translate-y-[1px] ${sizeStyles[size]} ${colorStyles[color]} ${className ?? ""}`}
+            className={`inline-flex items-center justify-center shrink-0 align-middle text-sm font-medium whitespace-nowrap border rounded-md outline-none transition-[color,box-shadow,translate] cursor-pointer focus-visible:ring-3 active:translate-y-[1px] ${sizeStyles[size]} ${colorStyles[color]} ${className ?? ""}`}
             {...buttonProps}
         >
             {leftSection && <span data-section="left">{leftSection}</span>}
